0b864b58ac8d916fef61c590a5f350f2e1448e8f
Shivani Deshmukh <162870619+shivanid2406@users.noreply.github.com>
2024-04-21 20:52:42 +0530

    Update PostRepository.java

--- src/main/java/com/rest/webservices/course/jpa/PostRepository.java
+++ src/main/java/com/rest/webservices/course/jpa/PostRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rest.webservices.users.Post;
 
-public interface PostRepository extends JpaRepository<Post, String>{
+public interface PostRepository extends JpaRepository<Post, Integer>{
 
 }
